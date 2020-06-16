7cd5d4e24d5bd5b20c3ed2036b855dc46e41f4d2
Pedro Cuesta Rivas <pacuestar@gmail.com>
2020-06-16 02:27:27 -0500

    finalizada la homework 07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,32 +1,45 @@
 // Do not change any of the function names
 
 function invocarCallback(cb) {
-  // Invoca al callback `cb`
+    // Invoca al callback `cb`
+    cb();
 }
 
 function sumarArray(numeros, cb) {
-  // Suma todos los números enteros (int/integers) de un array ("numeros")
-  // Pasa el resultado a `cb`
-  // No es necesario devolver nada
+    // Suma todos los números enteros (int/integers) de un array ("numeros")
+    // Pasa el resultado a `cb`
+    // No es necesario devolver nada
+    let suma = numeros.reduce(function (acc, el) {
+      return acc + el;
+    });
+    cb(suma);
 }
 
 function forEach(array, cb) {
-  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
-  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+    // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+    // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+    array.forEach(function(elemento){
+      cb(elemento);
+    });
 }
 
 function map(array, cb) {
-  // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
-  // El nuevo array debe tener la misma longitud que el array del argumento
+    // Crea un nuevo array
+    // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+    // El nuevo array debe tener la misma longitud que el array del argumento
+    
+    let arraynuevo = array.map(function(elemento){
+        return cb(elemento);
+    });
+    return arraynuevo;
 }
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
-  invocarCallback,
-  sumarArray,
-  forEach,
-  map,
+    invocarCallback,
+    sumarArray,
+    forEach,
+    map,
 };
---
+++ 07-JS-VI/homework/solucion.md
@@ -0,0 +1,3 @@
+* Funciones Callback
+
+    Una funcion Callback es una función que recibe por parámetro, otra función. Y, en su código interno llama o invoca a la función.
